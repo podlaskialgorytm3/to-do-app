385514c1145d5938d4a8fc99d2ca803eed5815d0
podlaskialgorytm <live.m851@gmail.com>
2025-02-09 20:41:02 +0100

    fix: Reapir gitignore again

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM eclipse-temurin:17-jdk-alpine
-
-WORKDIR /app
-
-COPY . /app
-
-RUN javac App.java
-
-CMD ["java", "App"]
--- src/App.java
+++
@@ -1,7 +0,0 @@
-import features.registration;
-public class App {
-    public static void main(String[] args) throws Exception {
-        registration.gettingRegistationData();
-        
-    }
-}
