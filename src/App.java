25a14965b60493f6fcb82db6761d8ad3df5ad378
podlaskialgorytm <live.m851@gmail.com>
2025-02-12 20:00:34 +0100

    feat: remove menu class

--- src/App.java
+++ src/App.java
@@ -1,9 +1,7 @@
-import features.Menu;
 //import features.task;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Menu.chooseMainOption();
         //task.gettingDataToTask();
     }
 }
