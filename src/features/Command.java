cefa38306777b8472715bc58c47e90b2b77f584f
podlaskialgorytm <live.m851@gmail.com>
2025-02-16 19:52:46 +0100

    feat: add special exepction to Command

--- src/features/Command.java
+++ src/features/Command.java
@@ -12,46 +12,42 @@ public class Command {
     public static final Command CODE = new Command("-c", true, value);
     public static final Command HELP = new Command("-h", false, null);
     public static final Command TASK = new Command("-t", false, null);
-    
+
     private Command(String command, boolean isValue, String value) {
         this.command = command;
         this.isValue = isValue;
     }
 
-    public static void readCommands(String[] args) throws IOException{
+    public static void readCommands(String[] args) throws IOException {
         boolean isCorrectCode = AccessCode.isCorrectCode(getCode(args));
-        for(int i = 0; i < args.length; i++){
-            if(args[i].equals(LOGIN.command)){
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals(LOGIN.command)) {
                 Login.login();
             }
-            if(args[i].equals(REGISTER.command)){
+            if (args[i].equals(REGISTER.command)) {
                 Registration.gettingRegistationData();
             }
-            if(args[i].equals(CODE.command)){
-                value = args[i+1];
+            if (args[i].equals(CODE.command)) {
+                value = args[i + 1];
                 isCorrectCode = AccessCode.isCorrectCode(Integer.parseInt(value));
             }
-            if(args[i].equals(HELP.command)){
+            if (args[i].equals(HELP.command)) {
                 help();
                 break;
             }
-            if(args[i].equals(TASK.command) && args[i+1].equals(HELP.command)){
-                helpTask();
-                break;
-            }
-            if(args[i].equals(TASK.command)){
-                if(isCorrectCode){
-                    Task task = new Task(getCode(args));
+            if (args[i].equals(TASK.command)) {
+                if (isCorrectCode) {
+                    String userId = User.getUserIdByCode(getCode(args));
+                    Task task = new Task(null, userId, null, null, null);
                     task.gettingDataToTask();
-                }
-                else{
+                } else {
                     System.out.println("You have to log in first!");
                 }
             }
         }
     }
 
-    public static void help(){
+    public static void help() {
         System.out.println("Usage: java <jar-file> [options]");
         System.out.println("Options:");
         System.out.println("  -l\t\t\tLogin");
@@ -61,19 +57,19 @@ public class Command {
         System.out.println("  -t\t\t\tTask (Check also task command -t -h)");
     }
 
-    public static void helpTask(){
+    public static void helpTask() {
         System.out.println("Usage: java <jar-file> -t [options]");
         System.out.println("Options:");
         System.out.println(" -t -c\t\tCreate task");
         System.out.println(" -t -h\t\tHelp");
-        System.out.println(" -t -s\\t\\Show tasks");
+        System.out.println(" -t -s\t\t\\Show tasks");
 
     }
 
-    public static int getCode(String[] args){
-        for(int i = 0; i < args.length; i++){
-            if(args[i].equals(CODE.command)){
-                value = args[i+1];
+    public static int getCode(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals(CODE.command)) {
+                value = args[i + 1];
                 return Integer.parseInt(value);
             }
         }
